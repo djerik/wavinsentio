d7cdb98ccf81457edf1159c0649b5ca668e3357c
Tobias Laursen <tla@dkcompany.com>
2021-04-08 00:27:17 +0200

    Added set temperature, set profile and token refresh logic

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(filename):
 
 setup(
     name="wavinsentio",
-    version="0.1.0",
+    version="0.2.0",
     url="https://github.com/djerik/wavinsentio",
     license='MIT',
 
--- wavinsentio/__init__.py
+++ wavinsentio/__init__.py
@@ -1,5 +1,5 @@
 """wavinsentio - API Wrapper for Wavin Sentio underfloor heating system"""
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __author__ = 'Tobias Laursen <djerik@gmail.com>'
 __all__ = []
